1e7b0c913c11ab62f354cae189f44beeb307ea49
scelsic2 <scelsic2@gmail.com>
2023-08-22 15:54:51 -0400

    working on question css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,21 @@
+const startRandomDiv = document.querySelector('.start-random-div')
 const startRandomBtn = document.querySelector('.start-random-btn')
+const questionDiv = document.querySelector('.question-div')
 const questionCategoryP = document.querySelector('.question-category-p')
-const questionP = document.querySelector('.question-p')
+const questionDifficultyP = document.querySelector('.question-difficulty-p')
+const questionH2 = document.querySelector('.question-h2')
 const choicesDiv = document.querySelector('.choices-div')
+const choiceOne = document.querySelector('#choice1')
+const choiceTwo = document.querySelector('#choice2')
+const choiceThree = document.querySelector('#choice3')
+const choiceFour = document.querySelector('#choice4')
 
 const randomAPI_URL = 'https://opentdb.com/api.php?amount=10'
 
 function startRandomGame() {
-    console.log('game started')
+    startRandomDiv.classList.add('hide-me')
+    questionDiv.classList.remove('hide-me') 
+    
     fetch(randomAPI_URL)
       .then(response => response.json())
       .then(data => {
@@ -16,8 +25,15 @@ function startRandomGame() {
         const questionCategory = data.results[0].category
         questionCategoryP.innerText = questionCategory
 
-        const question = data.results[0].question
-        questionP.innerText = question
+        const questionDifficulty = data.results[0].difficulty
+        questionDifficultyP.innerText = 'Difficulty: ' + questionDifficulty
+
+        let question = data.results[0].question;
+        question = question.replace(/&#039;/g, `'`);
+        question = question.replace(/&rsquo;/g, `'`);
+        question = question.replace(/&quot;/g, `"`);    
+        
+        questionH2.innerText = question
 
         const incorrectAnswers = data.results[0].incorrect_answers
         const correctAnswer = data.results[0].correct_answer
@@ -29,6 +45,19 @@ function startRandomGame() {
         const shuffledAnswerArray = incorrectAnswers.sort()
         console.log('----shuffledAnswerArray-----')
         console.log(shuffledAnswerArray)
+
+        choiceOne.innerText = shuffledAnswerArray[0]
+        choiceTwo.innerText = shuffledAnswerArray[1]
+
+        if (shuffledAnswerArray[2] != undefined) {
+            choiceThree.innerText = shuffledAnswerArray[2]
+        }  
+
+        if (shuffledAnswerArray[3] != undefined) {
+            choiceFour.innerText = shuffledAnswerArray[3]    
+        }
+        
+
       })
       .catch(err => {
         console.error(err)
